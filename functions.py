b35b334b2604001aacd2566c8a0ee9e00433979f
darinmilner <darin.milner@gmail.com>
2023-11-24 22:26:31 +0700

    functions.py update

--- functions.py
+++ functions.py
@@ -3,8 +3,20 @@ num_list = [1,2,3,4]
 def multiply(el):
     return el * 2 
 
+a = multiply(2)
+print(f"a {a}")
 newmap = map(multiply, num_list)
 print(list(newmap))
+
+regions = ["useast1", "uswest2", "apsouth1"]
+
+def attach_name(region):
+    return f"ds-core-{region}"
+
+region_names = map(attach_name, regions)
+
+for region in region_names:
+    print(region)
 
 # lambda
 print(list(map(lambda el: el * 2 , num_list)))
